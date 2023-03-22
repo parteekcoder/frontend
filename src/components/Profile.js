4ca4f0c3780a1f315c98866622238a0e9f994154
parteekcoder <parteekgoyal118@gmail.com>
2023-03-22 11:25:20 +0530

    fixing research profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -193,7 +193,7 @@ function Profile({peopleType}) {
                                         <div className='p-2 mt-4'>
                                             {active === 0 && <PersonalDetails edit={edit} data={data?.data[0]} />}
                                             {active === 1 && <Otherprofilelink edit={edit} isLogin={isLogin} data={data?.data[0]} />}
-                                            {active === 2 && <ResearchProfile edit={edit} isLogin={isLogin} data={data?.data[0]['research_profile']}/>}
+                                            {active === 2 && <ResearchProfile edit={edit} isLogin={isLogin} data={data?.data[0]['research_profile']} faculty={data?.data[0]}/>}
                                             {active > 2 && <BaseTable edit={edit} tablehead={Link[active].thead} faculty={data?.data[0]} data={data?.data[0][map[Link[active].Title]]} Editfeild={Editfeild} HandleEdit={HandleEdit} feild={Link[active].feild} isLogin={isLogin} title={map[Link[active].Title]}/>}
                                             {/* <BaseTable edit={edit} /> */}
                                         </div>
