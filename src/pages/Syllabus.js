58757618002d404d08155739d3c48a7df73f0d91
Ayush-13-02 <ayushsharma13jun@gmail.com>
2023-04-08 19:38:27 +0530

    minor changes

--- src/pages/Syllabus.js
+++ src/pages/Syllabus.js
@@ -8,7 +8,7 @@ function Syllabus() {
 
     const {data} = useFetch(`/dept/${useParams()?.dept}/Syllabus`);
     const ref= useRef("B.Tech");
-
+    console.log(data)
     return (
         <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-4 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
             <Heading name="Syllabus" />
