fada404c91e6acdbb7554f443a0a04a1e0c4fa2f
parteekcoder <parteekgoyal118@gmail.com>
2023-04-08 20:42:14 +0530

    Update Students.js

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,21 +1,22 @@
 import { React, useState } from 'react'
 import useFetch from '../hooks/useFetch'
 import OpenPdf from './OpenPdf'
-import { useLocation } from 'react-router-dom';
-import IT7 from '../components/Pdf/IT7.pdf'
 import Heading from '../components/Heading';
+import { useParams } from 'react-router-dom';
 function Students() {
 
-    const [url, setUrl] = useState(useLocation());
-    const { data, loading, error, reFetch } = useFetch(url.pathname);
-
+ 
+    const [state,setState] = useState(2021);
+    const {data} = useFetch(`/dept/${useParams()?.dept}/Student?q=${state}`);
     return (
         <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-4 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
             <Heading name="Students" />
             <div>
                 <div className='flex w-fit items-center my-1 border border-gray-300 text-gray-900 text-sm p-2 rounded'>
                     <label htmlFor="states" className="mr-2">Branch :</label>
-                    <select id="states" className="border-none outline-none">
+                    <select id="states" className="border-none outline-none" onChange={(e) =>{
+                        setState(e.target.value);
+                    }}>
                         <option defaultValue={2021}>{2021}</option>
                         <option value={2021}>2021</option>
                         <option value={2020}>2020</option>
@@ -23,7 +24,7 @@ function Students() {
                         <option value={2018}>2018</option>
                     </select>
                 </div>
-                <OpenPdf link={IT7} />
+                <OpenPdf link={data?.link} />
             </div>
         </div>
     )
