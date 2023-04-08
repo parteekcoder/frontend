1ced05c0c2b073fb4592655823abf6cd0347e6fe
YASH-GU24 <yashg.it.20@nitj.ac.in>
2023-04-08 18:50:54 +0530

    Changes

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -5,7 +5,7 @@ import useFetch from '../hooks/useFetch'
 import { useParams } from 'react-router-dom'
 
 function Publications() {
-    const tablehead = ['Document title', "Authors", "Year", "Source", "Cited  by"]
+    const tablehead = ['Document title', "Authors", "Year", "Source", "Status"]
     const {data} = useFetch(`/dept/${useParams()?.dept}/Publications`)
     return (
         <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-4 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
