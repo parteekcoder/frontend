e1e92c4082a2c40936f1f2f6b9600e6f0dffd194
Aditya Garg <uber6707@gmail.com>
2023-04-09 04:19:01 +0530

    updated calendar

--- src/pages/AcademicCalender.js
+++ src/pages/AcademicCalender.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 import OpenPdf from './OpenPdf'
 import Heading from '../components/Heading'
 import useFetch from '../hooks/useFetch'
+import { useParams } from 'react-router-dom'
 
 function AcademicCalender() {
-    const data = useFetch('/deptCalendar/get/all').data;
+    const data = useFetch(`/dept/${useParams()?.dept}/deptCalendar`).data;
     console.log(data);
     return (
         <div className='w-[98%] rounded-[9px] border border-[rgba(0,105,140,0.2)] p-4 mx-1 xl:mx-3 my-[60px] pt-[54px] place-items-center'>
