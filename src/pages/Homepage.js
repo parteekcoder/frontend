7f287c0b3ed6c8d35f7e45d8be3a8ed5c829799f
Aditya Garg <uber6707@gmail.com>
2023-04-09 03:18:06 +0530

    frontend cookie

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import { Department } from '../components/department'
-import Imagesroll from '../components/Imagesroll'
-import TopPlacement from '../components/TopPlacement'
+import { Department } from "../components/department";
+import Imagesroll from "../components/Imagesroll";
+import TopPlacement from "../components/TopPlacement";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Homepage() {
+  const params = useParams();
+
+  useEffect(() => {
+    const sessionID = params?.id;
+
+    if (sessionID != null) {
+      var now = new Date();
+      var time = now.getTime();
+      var expireTime = time + 90*24*60*60;
+      now.setTime(expireTime);
+      document.cookie = `session=${sessionID};expires=${now.toUTCString()};path=/`;
+      console.log(document.cookie);
+    }
+  }, []);
+
   return (
-    <div className='flex items-center flex-col w-full h-full px-auto mt-0'>
-        <Imagesroll/>
-        <Department/>
-        <TopPlacement/>
+    <div className="flex items-center flex-col w-full h-full px-auto mt-0">
+      <Imagesroll />
+      <Department />
+      <TopPlacement />
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
