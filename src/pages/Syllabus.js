9f71860bec324144b9fdac1c55fb4d14cfca8d9f
Ayush-13-02 <ayushsharma13jun@gmail.com>
2023-04-10 07:40:18 +0530

    minor changes

--- src/pages/Syllabus.js
+++ src/pages/Syllabus.js
@@ -1,5 +1,4 @@
 import React, { useRef } from 'react'
-import syllabuspdf from '../components/Pdf/syllabus.pdf'
 import OpenPdf from './OpenPdf'
 import Heading from '../components/Heading'
 import { useParams } from 'react-router-dom'
